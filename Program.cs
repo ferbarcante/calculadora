3850449ca66237a15be19525ac90686a0b5f6ff8
ferbarcante <barcantef@gmail.com>
2025-02-20 15:41:42 -0300

    multiplicacao

--- Program.cs
+++ Program.cs
@@ -36,6 +36,36 @@ namespace Calculator
             Console.WriteLine("Resultado: " + total);
         }
 
+        static void Divisao()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Digite o dividendo: ");
+            float dividendo = float.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o divisor: ");
+            float divisor = float.Parse(Console.ReadLine());
+
+            if (divisor == 0)
+            {
+                Console.WriteLine("Não é possível dividir por zero!");
+                return;
+            }
+
+            float total = dividendo / divisor;
+            Console.WriteLine("Resultado: " + total);
+        }
         
+        static void Multiplicacao()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Digite o primeiro valor: ");
+            float primeiroValor = float.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o segundo valor: ");
+            float segundoValor = float.Parse(Console.ReadLine());
+
+            float total = primeiroValor * segundoValor;
+            Console.WriteLine("Resultado: " + total);
+        }
     }
 }
